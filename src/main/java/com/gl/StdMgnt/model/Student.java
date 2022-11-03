c0b88d76303942c08b71695a9793ea7dd4bfa7d4
prabhu <prabhu_tc@yahoo.com>
2022-11-03 21:41:15 +0530

    [Updating Student.java]

--- src/main/java/com/gl/StdMgnt/model/Student.java
+++ src/main/java/com/gl/StdMgnt/model/Student.java
@@ -4,62 +4,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "students")
 public class Student {
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+	@GenericGenerator(name = "native", strategy = "native")
 	private int id;
+
 	private String name;
+
 	private String department;
+
 	private String country;
-
-	public Student(String name, String department, String country) {
-		super();
-		this.name = name;
-		this.department = department;
-		this.country = country;
-	}
-
-	public Student() {
-		super();
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", department=" + department + ", country=" + country + "]";
-	}
 
 }

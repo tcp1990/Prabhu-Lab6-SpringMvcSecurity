fc2c8ba48d248469c3a04e38a62208862777bc34
prabhu <prabhu_tc@yahoo.com>
2022-11-04 18:45:35 +0530

    [Adding security/SecurityWebApplicationInitializer.java and WebSecurityConfiguration.java]

---
+++ src/main/java/com/gl/StdMgnt/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.gl.StdMgnt.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/com/gl/StdMgnt/security/WebSecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.gl.StdMgnt.security;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = passwordEncoder();
+		auth.inMemoryAuthentication().withUser("user") // #1
+				.password(encoder.encode("password")).roles("USER").and().withUser("admin") // #2
+				.password(encoder.encode("password")).roles("ADMIN");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**"); // #3
+	}
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers(POST,"/students/save").hasAnyRole("USER","ADMIN")
+            .antMatchers("/","/students/showFormForAdd","/students/403").hasAnyRole("USER","ADMIN")
+            .antMatchers("/students/update","/students/delete").hasAnyRole("ADMIN")
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginProcessingUrl("/login").successForwardUrl("/").permitAll()
+            .and()
+            .logout().logoutSuccessUrl("/login").permitAll()
+            //.and()
+            //.exceptionHandling().accessDeniedPage("/student/403")
+            .and()
+            .cors().and().csrf().disable();
+    }
+}
